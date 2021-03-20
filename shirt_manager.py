9c5c7b222dd2fce46a85c7e076634c43b25fbfb7
mitcha871 <mitchaoki@gmail.com>
2021-03-20 22:09:33 +0900

    before data structure change

--- shirt_manager.py
+++ shirt_manager.py
@@ -1,31 +1,55 @@
 import os
 import json
 
+class Order():
+    def __init__(self, name, size, text, orderID, status='PENDING'):
+        self.orderID = orderID
+        self.status = status
+        self.name = name
+        self.size = size
+        self.text = text
+
 def clr_scrn():
     os.system("clear")
 
-def init_data(SAVE_FILE_NAME):
-    if not os.path.isfile(SAVE_FILE_NAME):
-        with open(SAVE_FILE_NAME, 'w') as save_file:
-            json.dump([], save_file)
-    return json.load(open(SAVE_FILE_NAME))
+def display_orders(orders):
+    print("We have these orders in the system:\n")
+    print("{:<10}\t{:<15}\t{:<15}\t{:<15}\t{:<15}".format("Order ID", "Order Status", "Client", "Shirt size", "Custom Text"))
+    for order in orders:
+        print("{:<15}\t{:<15}\t{:<15}\t{:<15}\t{:<15}\t".format(order.orderID, order.status, order.name, order.size, order.text))
+    print("")
 
-def order_shirt():
-    confirm = ''
-    while confirm.lower() != 'y':
+def main_menu(orders, VALID_CHOICES):
+    choice = ""
+    while choice not in VALID_CHOICES.keys():
         clr_scrn()
-        order = get_order()
-        print(f"Name: {order['name']}\nShirt size: {order['size']}\nText: {order['text']}")
-        confirm = input("Is this OK? Input 'y' to continue or any other key to try again ")
-    return order
+        display_orders(orders)
+        print("What do you want to do?")
+        for number, choice in VALID_CHOICES.items():
+            print(f"{number}: {choice}")
+        choice = input()
+    return choice
 
-def get_order():
-    order = {}
-    order['status'] = 'PENDING'
-    order['name'] = valid_string("What's your name? ")
-    order['size'] = valid_size(VALID_SHIRT_SIZES)
-    order['text'] = valid_string("What do you want it to say on the front? ")
-    return order
+def new_order(orders):
+    confirm = ''
+    while confirm.lower() not in ['y', 'q']:
+        clr_scrn()
+        display_orders(orders)
+        name = valid_string("What's the name of the client? ")
+        size = valid_size(VALID_SHIRT_SIZES)
+        text = valid_string("What do they want it to say on the front? ")
+        print("")
+        display_input("Name: ", name)
+        display_input("Shirt Size: ", size)
+        display_input("Custom Text: ", text) 
+        confirm = input("\nIs this OK? Input 'y' to continue, 'q' to cancel, or any other key to try again: ")
+    if confirm.lower() == 'y':
+        orderID = max(get_orderIDs(orders)) + 1
+        orders.append(Order(name, size, text, orderID))
+        persist_orders(SAVE_FILE_NAME, orders)
+
+def display_input(field, text):
+    print("{:>13}{:<16}".format(field, text))
 
 def valid_string(prompt):
     while True:
@@ -33,64 +57,65 @@ def valid_string(prompt):
         if len(user_input) < 16:
             break
         else:
-            clr_scrn()
             print("Please enter valid text less than 16 characters")
     return user_input
 
 def valid_size(VALID_SHIRT_SIZES):
     user_input = ""
     while user_input not in VALID_SHIRT_SIZES:
-        print(f"What size shirt do you want? ({', '.join(VALID_SHIRT_SIZES)})")
+        print(f"What size shirt do they need? ({', '.join(VALID_SHIRT_SIZES)}) ", end = '')
         user_input = input()
     return user_input
 
-def persist_orders(SAVE_FILE_NAME, orders):
-    with open(SAVE_FILE_NAME, 'w') as save_file:
-        json.dump(orders, save_file, indent=2)
-
-def display_orders(orders):
-    print("We have these orders in the system:\n")
-    print("{:<16}\t{:<16}\t{:<16}\t{:<16}".format("Order Status", "Client", "Shirt size", "Custom Text"))
+def get_orderIDs(orders):
+    orderIDs = [0]
     for order in orders:
-        print("{:<16}\t{:<16}\t{:<16}\t{:<16}".format(*order.values()))
-    input("\nPress Enter to return to the menu")
+        orderIDs.append(int(order.orderID))
+    return orderIDs
 
-def select_action(VALID_CHOICES):
-    choice = ""
-    while choice not in VALID_CHOICES.keys():
-        clr_scrn()
-        print("What do you want to do?")
-        for number, choice in VALID_CHOICES.items():
-            print(f"{number}: {choice}")
-        choice = input()
-    return choice
+def persist_orders(SAVE_FILE_NAME, orders):
+    order_list = []
+    for order in orders:
+        data = {}
+        data["orderID"] = order.orderID
+        data["status"] = order.status
+        data["name"] = order.name
+        data["size"] = order.size
+        data["text"] = order.text
+        order_list.append(data)
+    with open(SAVE_FILE_NAME, 'w') as save_file:
+        json.dump(order_list, save_file, indent=2)
+
+def import_orders(SAVE_FILE_NAME):
+    orders = []
+    if os.path.isfile(SAVE_FILE_NAME):
+        with open(SAVE_FILE_NAME) as save_file:
+            order_list = json.load(save_file)
+        for order in order_list:
+            orders.append(Order(order['name'], order['size'], order['text'], order['orderID'], order['status']))
+    return orders
 
 # Iniitalize constants and variables, and load save
-VALID_CHOICES = {"1": "Order shirt",
-                 "2": "Check orders",
-                 "3": "Exit"}
+VALID_CHOICES = {"1": "Input new order",
+                 "q": "Exit"}
 VALID_SHIRT_SIZES = ['small', 'medium', 'large']
 SAVE_FILE_NAME = "orders.sav"
 
 # Print welcome message & load save file
 clr_scrn()
-print("\nWelcome to my shirt maker program.")
-print("I'm using it to practice loops, functions, and storing and retrieving data\n")
+print("\nWelcome to my shirt production management program.")
+print("I'm using it to practice basic classes, loops, functions, and storing and retrieving data\n")
 input("Press any key to continue")
 
-orders = init_data(SAVE_FILE_NAME)
+orders = import_orders(SAVE_FILE_NAME)
 
 # Main loop
 while True:
     clr_scrn()
-    choice = select_action(VALID_CHOICES)
+    choice = main_menu(orders, VALID_CHOICES)
     if choice == '1':
         clr_scrn()
-        orders.append(order_shirt())
-        persist_orders(SAVE_FILE_NAME, orders)
-    elif choice == '2':
-        clr_scrn()
-        display_orders(orders)
-    elif choice == '3':
+        new_order(orders)
+    elif choice == 'q':
         print("Thanks for using this program!")
         break
